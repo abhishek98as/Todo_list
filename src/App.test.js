3924dea39585a5bd67ada04a5d9822c73600c645
ABHISHEK SINGH <49355119+abhishek98as@users.noreply.github.com>
2023-06-13 23:49:39 +0530

    hello

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<todo />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
